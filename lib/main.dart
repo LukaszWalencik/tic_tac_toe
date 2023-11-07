5e224e7386cd2f819d20428e4fbf55ce444f7ddd
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-07 20:24:36 +0100

    Change app name, turn off debug banner and use as home of MaterialApp our MainMenuScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/screens/main_menu_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
+        title: 'TicTacToe',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const Placeholder());
+        home: MainMenuScreen());
   }
 }
