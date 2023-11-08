34a06ab241f960fa686afd3cbeb4fba44e0255c9
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-08 20:54:43 +0100

    Add two CustomButtons

--- lib/screens/main_menu_screen.dart
+++ lib/screens/main_menu_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/widgets/custom_button.dart';
 
 class MainMenuScreen extends StatelessWidget {
   static String routeName = "/main-menu";
@@ -7,8 +8,16 @@ class MainMenuScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Main Menu'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomButton(onTap: () {}, text: 'Create Room'),
+            const SizedBox(height: 10),
+            CustomButton(onTap: () {}, text: 'Join Room')
+          ],
+        ),
       ),
     );
   }
