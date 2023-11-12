9f20a278b1438faee598dd451708a835182a17c0
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-12 21:56:52 +0100

    Change CreateRoomScreen from stateless to stateful and create simple screen

--- lib/screens/create_room_screen.dart
+++ lib/screens/create_room_screen.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/responsive/responsive.dart';
+import 'package:tic_tac_toe/widgets/custom_text.dart';
 
-class CreateRoomScreen extends StatelessWidget {
+class CreateRoomScreen extends StatefulWidget {
   static String routeName = '/create';
   const CreateRoomScreen({super.key});
 
   @override
+  State<CreateRoomScreen> createState() => _CreateRoomScreenState();
+}
+
+class _CreateRoomScreenState extends State<CreateRoomScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Responsive(
+          child: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CustomText(
+                  text: 'Create Room',
+                  fontSize: 50,
+                  shadows: [Shadow(blurRadius: 40, color: Colors.blue)]),
+            ],
+          ),
+        ),
+      )),
+    );
   }
 }
