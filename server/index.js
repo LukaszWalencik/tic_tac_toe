c855c521716cee61a4aa3050e01d2e011b760e1e
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-14 18:51:12 +0100

    Setup MongoDB atlas and inject path to the server into our index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const socket = require('socket.io');
 const http = require('http');
-
+const mongoose = require('mongoose');
 //Initialize express
 const app = express();
 
@@ -12,6 +12,16 @@ var io = socket(server);
 // Middle wear : manipulate data coming from client to server
 app.use(express.json()); //It will convert all incoming data to json format
 
+const DB = "mongodb+srv://lukasz:Falubaz21@cluster0.uqw5lmr.mongodb.net/tictactoe"
+
+mongoose.connect(DB).then(()=> {
+    console.log("MongoDB connection successfull");
+}).catch((e)=>{
+    console.log(e);
+});
+
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, '0.0.0.0', ()=> {console.log('Server running on port ${PORT}');})
+server.listen(PORT, '0.0.0.0', ()=> {
+    console.log('Server running on port ${PORT}');
+});
