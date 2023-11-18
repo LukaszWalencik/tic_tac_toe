65f71da83e12f6d0ab7f2a1ea965fd6ddfd2ef0d
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-18 16:52:24 +0100

    Fix bug and define Room variable in index.js before use

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const socket = require('socket.io');
 const http = require('http');
 const mongoose = require('mongoose');
+const Room = require('./models/room');
 //Initialize express
 const app = express();
 
