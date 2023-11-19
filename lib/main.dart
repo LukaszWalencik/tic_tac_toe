30995501c41ec6b094624e8785a2985b2520137a
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-19 20:59:05 +0100

    Wrap MaterialApp with ChangeNotifierProvider using RoomDataProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tic_tac_toe/providers/room_data_provider.dart';
 import 'package:tic_tac_toe/screens/create_room_screen.dart';
 import 'package:tic_tac_toe/screens/game_screen.dart';
 import 'package:tic_tac_toe/screens/join_room_screen.dart';
@@ -14,20 +16,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TicTacToe',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => RoomDataProvider(),
+      child: MaterialApp(
+        title: 'TicTacToe',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routes: {
+          MainMenuScreen.routeName: (context) => const MainMenuScreen(),
+          CreateRoomScreen.routeName: (context) => const CreateRoomScreen(),
+          JoinRoomScreen.routeName: (context) => const JoinRoomScreen(),
+          GameScreen.routeName: (context) => const GameScreen(),
+        },
+        initialRoute: MainMenuScreen.routeName,
       ),
-      routes: {
-        MainMenuScreen.routeName: (context) => const MainMenuScreen(),
-        CreateRoomScreen.routeName: (context) => const CreateRoomScreen(),
-        JoinRoomScreen.routeName: (context) => const JoinRoomScreen(),
-        GameScreen.routeName: (context) => const GameScreen(),
-      },
-      initialRoute: MainMenuScreen.routeName,
     );
   }
 }
