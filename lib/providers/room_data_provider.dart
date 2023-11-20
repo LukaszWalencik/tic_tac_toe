dcb6cd8f2df4a779d638473abefeed04113673c5
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-20 20:15:27 +0100

    In RoomDataProvider we use two getters for two PlayerModels (player1 and player 2) also create 2 functions to update Data for both of them

--- lib/providers/room_data_provider.dart
+++ lib/providers/room_data_provider.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/models/player_model.dart';
 
 class RoomDataProvider extends ChangeNotifier {
   Map<String, dynamic> _roomData = {};
   Map<String, dynamic> get roomData => _roomData;
 
+  PlayerModel _playerModel1 =
+      PlayerModel(playerName: '', socketID: '', points: 0, playerType: ')');
+
+  PlayerModel _playerModel2 =
+      PlayerModel(playerName: '', socketID: '', points: 0, playerType: 'X');
+
+  PlayerModel get playerModel1 => _playerModel1;
+  PlayerModel get playerModel2 => _playerModel2;
+
   void updateRoomData(Map<String, dynamic> data) {
     _roomData = data;
+    notifyListeners();
+  }
+
+  void updatePlayer1(Map<String, dynamic> player1Data) {
+    _playerModel1 = PlayerModel.fromMap(player1Data);
+    notifyListeners();
+  }
+
+  void updatePlayer2(Map<String, dynamic> player2Data) {
+    _playerModel2 = PlayerModel.fromMap(player2Data);
     notifyListeners();
   }
 }
