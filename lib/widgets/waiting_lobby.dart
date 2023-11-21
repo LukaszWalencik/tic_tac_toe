d0eb54c9e9dc61a472a105eb5b267f1327db65ab
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-21 21:10:13 +0100

    Add late idController as TextEditingController and give him value of room id in init state (after all dispose controller)

--- lib/widgets/waiting_lobby.dart
+++ lib/widgets/waiting_lobby.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tic_tac_toe/providers/room_data_provider.dart';
 
 class WaitingLobby extends StatefulWidget {
   const WaitingLobby({super.key});
@@ -8,8 +10,23 @@ class WaitingLobby extends StatefulWidget {
 }
 
 class _WaitingLobbyState extends State<WaitingLobby> {
+  late TextEditingController idController = TextEditingController();
+
+  @override
+  void initState() {
+    idController =
+        Provider.of<RoomDataProvider>(context, listen: false).roomData['_id'];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  @override
+  void dispose() {
+    idController.dispose();
+    super.dispose();
   }
 }
