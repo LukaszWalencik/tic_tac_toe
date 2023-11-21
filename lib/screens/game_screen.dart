a5195559713934897e587c9d957f0e5bdbd6459a
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-21 21:57:12 +0100

    Inject SockedMethod into GameScreen to use listeners
    Show WaitingLobby or Text widget depense of leisteners results

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tic_tac_toe/providers/room_data_provider.dart';
+import 'package:tic_tac_toe/resources/socket_methods.dart';
+import 'package:tic_tac_toe/widgets/waiting_lobby.dart';
 
 class GameScreen extends StatefulWidget {
   static String routeName = '/game';
@@ -11,16 +13,23 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  final SocketMethod _socketMethod = SocketMethod();
+  @override
+  void initState() {
+    _socketMethod.updateRoomListener(context);
+    _socketMethod.updatePlayerListener(context);
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    RoomDataProvider roomDataProvider = Provider.of<RoomDataProvider>(context);
     return Scaffold(
-      body: Center(
-        child: Text(
-          Provider.of<RoomDataProvider>(context, listen: false)
-              .roomData
-              .toString(),
-        ),
-      ),
-    );
+        body: roomDataProvider.roomData['isJoin']
+            ? const WaitingLobby()
+            : const Center(
+                child: Text('Game Start'),
+              ));
   }
 }
