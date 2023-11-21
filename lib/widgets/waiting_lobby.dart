ee60d47b19c4ce80798199946ef327e8f87d6f85
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-21 21:22:50 +0100

    Create Column with CircularProgressIndicator, Text widget with information about status of game and TextField with game room id

--- lib/widgets/waiting_lobby.dart
+++ lib/widgets/waiting_lobby.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tic_tac_toe/providers/room_data_provider.dart';
+import 'package:tic_tac_toe/widgets/custom_textfield.dart';
 
 class WaitingLobby extends StatefulWidget {
   const WaitingLobby({super.key});
@@ -21,7 +22,24 @@ class _WaitingLobbyState extends State<WaitingLobby> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(),
+          const SizedBox(height: 50),
+          const Text('Waiting for another player to join'),
+          const SizedBox(height: 20),
+          CustomTextField(
+            controller: idController,
+            hintText: '',
+            isReadOnly: true,
+          )
+        ],
+      ),
+    );
   }
 
   @override
