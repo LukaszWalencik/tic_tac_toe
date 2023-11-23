8782f97ea95c5d749419c8c55ee5e6654d1896b7
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-23 20:46:48 +0100

    Create TicTacToeBoard simple widget

---
+++ lib/widgets/tictacto_board.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TicTacToeBoard extends StatefulWidget {
+  const TicTacToeBoard({super.key});
+
+  @override
+  State<TicTacToeBoard> createState() => _TicTacToeBoardState();
+}
+
+class _TicTacToeBoardState extends State<TicTacToeBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
