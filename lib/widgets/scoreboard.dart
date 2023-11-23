05af8525dfebe2c01efd21e4d24e367657ce84ab
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-23 20:34:13 +0100

    Create Scoreboard for player two with name and score value taken from RoomDataProvider

--- lib/widgets/scoreboard.dart
+++ lib/widgets/scoreboard.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tic_tac_toe/providers/room_data_provider.dart';
 
 class Scoreboard extends StatelessWidget {
   const Scoreboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    RoomDataProvider roomDataProvider = Provider.of<RoomDataProvider>(context);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Text(
+                roomDataProvider.playerModel1.playername,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                roomDataProvider.playerModel1.points.toInt().toString(),
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
