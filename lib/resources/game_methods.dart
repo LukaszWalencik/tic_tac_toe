6a491e634f6b5801045e043674345b01bf0d216b
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-25 21:10:10 +0100

    Create GameMethods class to check winner of the game

---
+++ lib/resources/game_methods.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+import 'package:tic_tac_toe/providers/room_data_provider.dart';
+
+class GameMethods {
+  void checkWinner(BuildContext context, Socket socketClient) {
+    RoomDataProvider roomDataProvider =
+        Provider.of<RoomDataProvider>(context, listen: false);
+    String winner = ''; //Name of plater or player display element  'X' or 'O'
+  }
+}
