0956c8e9aef4aa61793c5a8367038bc18b8c437e
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-26 22:30:41 +0100

    change shema to schema

--- server/models/room.js
+++ server/models/room.js
@@ -1,21 +1,21 @@
 const mmongoose = require('mongoose');
-const playerShema = require('./player');
+const playerSchema = require('./player');
 const { default: mongoose } = require('mongoose');
 
 const roomSchema = new mmongoose.Schema(
     {// Ocupancy mean how many players can be in game room
-        occupancy:{type: Number, default: 2},
+        occupancy:{type: Number, default: 2,},
         // How long the game will after which we will stop it
-        maxRounds:{type: Number, default: 3},
+        maxRounds:{type: Number, default: 3,},
         // Which round we are now in the game
         currentRound:{require: true, type: Number, default: 1,},
-        players: [playerShema],
+        players: [playerSchema],
         // If room is joinable or not. When two players in room value will be false
         isJoin: {type: Boolean, default: true,},
         // which player turns is there and it will change every round
-        turn: playerShema,
+        turn: playerSchema,
         // whoever creates room, that players index will be 0 or else 1. This will help with tracking rounds
-        turnIndex: {type: Number,  default: 0,},
+        turnIndex: {type: Number,  default: 0},
     }
 )
 const roomModel = mongoose.model('Room', roomSchema);
