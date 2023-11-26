a96d080dab62816d1cc2d0906042d99230edfc13
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-26 22:29:39 +0100

    Fix bug name 'shema' to 'schema

--- server/models/player.js
+++ server/models/player.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const playerShema = new mongoose.Schema({
-    playerName: {type: String, trim: true},
+const playerSchema = new mongoose.Schema({
+    playername: {type: String, trim: true},
     socketID: {type: String},
     points: {type: Number, default: 0,},
     // X or O
     playerType: {require: true, type: String},
 })
-module.exports = playerShema;
+module.exports = playerSchema;
